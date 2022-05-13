8e10f85eece3c7a066c18127eac972c426d7e686
aishux <aishux07@gmail.com>
2022-05-13 22:05:15 +0530

    add, getall and get specific goods

--- Brownie/contracts/TheyCoin.sol
+++ Brownie/contracts/TheyCoin.sol
@@ -1,26 +1,38 @@
+// contracts/FarmToken.sol
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 
 contract TheyCoin is ERC20, ChainlinkClient {
-
     address owner;
     using Chainlink for Chainlink.Request;
-  
+
     uint256 public volume;
-    
+
     address private oracle;
     bytes32 private jobId;
     uint256 private fee;
 
     address token_address;
 
-    mapping(bytes32=>address) public requestId_toSender;
-    mapping(address=>uint256) public address_to_ethgiven;
+    struct Goods {
+        uint256 id;
+        string name;
+        uint256 token_amount;
+        string image_uri;
+        string description;
+    }
+
+    Goods[] public all_goods;
+
+    mapping(bytes32 => address) public requestId_toSender;
+    mapping(address => uint256) public address_to_ethgiven;
+    mapping(address => Goods[]) public seller_to_goods;
 
     constructor(uint256 initialSupply) ERC20("TheyCoin", "TC") {
         _mint(msg.sender, initialSupply);
@@ -31,42 +43,52 @@ contract TheyCoin is ERC20, ChainlinkClient {
         setPublicChainlinkToken();
         oracle = 0x3A56aE4a2831C3d3514b5D7Af5578E45eBDb7a40;
         jobId = "3b7ca0d48c7a4b2da9268456665d11ae";
-        fee = 0.01 * 10 ** 18;
+        fee = 0.01 * 10**18;
     }
 
     // Later remove this "_to_user" with msg.sender and also remove functional arg
-    function payUser (address _token, address _to_user) payable public {
+    function payUser(address _token, address _to_user) public payable {
         token_address = _token;
         bytes32 requestId = requestVolumeData();
         requestId_toSender[requestId] = _to_user;
         address_to_ethgiven[_to_user] = msg.value;
     }
 
-    function requestVolumeData() public returns (bytes32 requestId) 
-    {
-        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
-        
+    function requestVolumeData() public returns (bytes32 requestId) {
+        Chainlink.Request memory request = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfill.selector
+        );
+
         // Set the URL to perform the GET request on
-        request.add("get", "https://min-api.cryptocompare.com/data/price?fsym=INR&tsyms=ETH");
-        
+        request.add(
+            "get",
+            "https://min-api.cryptocompare.com/data/price?fsym=INR&tsyms=ETH"
+        );
+
         request.add("path", "ETH");
-        
+
         // Multiply the result by 1000000000000000000 to remove decimals
-        int timesAmount = 10**18;
+        int256 timesAmount = 10**18;
         request.addInt("times", timesAmount);
-        
+
         // Sends the request
         return sendChainlinkRequestTo(oracle, request, fee);
     }
-    
+
     /**
      * Receive the response in the form of uint256
-     */ 
-    function fulfill(bytes32 _requestId, uint256 _volume) public recordChainlinkFulfillment(_requestId)
+     */
+    function fulfill(bytes32 _requestId, uint256 _volume)
+        public
+        recordChainlinkFulfillment(_requestId)
     {
         volume = _volume;
 
-        uint256 amount_in_eth = address_to_ethgiven[requestId_toSender[_requestId]];
+        uint256 amount_in_eth = address_to_ethgiven[
+            requestId_toSender[_requestId]
+        ];
 
         require(amount_in_eth > 0, "Insufficient ETH");
 
@@ -76,9 +98,36 @@ contract TheyCoin is ERC20, ChainlinkClient {
 
         require(amount_of_tokens > 0, "Tokens too less!");
 
-        require(paymentToken.transferFrom(owner, requestId_toSender[_requestId], amount_of_tokens),"transfer Failed");
+        require(
+            paymentToken.transferFrom(
+                owner,
+                requestId_toSender[_requestId],
+                amount_of_tokens
+            ),
+            "transfer Failed"
+        );
 
         address_to_ethgiven[requestId_toSender[_requestId]] = 0;
     }
 
+    function addGoods(
+        address _seller_address,
+        uint256 _id,
+        string memory _name,
+        uint256 _token_amount,
+        string memory _image_uri,
+        string memory _description
+    ) public {
+        Goods memory new_good = Goods(_id, _name, _token_amount, _image_uri, _description);
+        seller_to_goods[_seller_address].push(new_good);
+        all_goods.push(new_good);
+    }
+
+    function getAllGoods() public view returns(Goods[] memory){
+        return all_goods;
+    }
+
+    function getGoodBySeller(address _seller_address) view public returns(Goods[] memory){
+        return seller_to_goods[_seller_address];
+    }
 }
--- Brownie/scripts/deploy.py
+++ Brownie/scripts/deploy.py
@@ -1,4 +1,4 @@
-from brownie import TheyCoin, accounts
+from brownie import TheyCoin, config, network
 from scripts.helpfulScripts import getAccount
 from web3 import Web3
 import requests
@@ -8,7 +8,7 @@ from scripts.helpfulScripts import fundWithLink
 def deploy_token():
     account = getAccount()
     initial_supply = Web3.toWei("10000", "ether")
-    our_token = TheyCoin.deploy(initial_supply, {"from": account}, publish_source=True)
+    our_token = TheyCoin.deploy(initial_supply, {"from": account}, publish_source=config["networks"][network.show_active()]["verify"])
     print(our_token)
 
 
@@ -25,6 +25,49 @@ def transfer_to_user(user_address, amount):
     print("Tokens will be sent soon!")
 
 
+def add_goods(seller_address, id, name, token_amount, image_uri, description):
+    account = getAccount()
+    our_token = TheyCoin[-1]
+    tx_add = our_token.addGoods(seller_address, id, name, token_amount, image_uri, description, {"from": account})
+    tx_add.wait(1)
+    print("Added the good!")
+
+
+def get_all_goods():
+    our_token = TheyCoin[-1]
+    print("All goods are:\n")
+    all_items = our_token.getAllGoods()
+    for item in all_items:
+        print(item)
+        print("\n")
+
+    # length = our_token.all_goods_length()
+    # for i in range(0, length):
+    #     print(our_token.all_goods(i))
+
+
+def get_good_by_seller(seller_address):
+    our_token = TheyCoin[-1]
+    print(f"\nAll goods of seller {seller_address} are:\n")
+    # length = our_token.seller_to_goods_length(seller_address)
+    # for i in range(0, length):
+    #     print(our_token.seller_to_goods(seller_address, i))
+
+    all_items = our_token.getGoodBySeller("")
+    for item in all_items:
+        print(item)
+        print("\n")
+
+
 def main():
-    deploy_token()
-    transfer_to_user("", 500)
+    # deploy_token()
+    
+    # transfer_to_user("", 500)
+
+    # add_goods("", 3, "Test good3", 15, "https://ipfs.io/ipfs/?filename=0-PUG.json", "This is third test")
+
+    get_all_goods()
+
+    # get_good_by_seller("")
+
+    # get_good_by_seller("")
